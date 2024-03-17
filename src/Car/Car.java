023219666a50aeaeb526c0a6fece33a535861ac4
DSM <Isaharoscf@gmail.com>
2024-03-17 18:27:06 +0300

    Ready

--- src/Car/Car.java
+++ src/Car/Car.java
@@ -1,49 +1,25 @@
 package Car;
-abstract class Car1 {
-     private Engine engine;
-     private Wheel wheels;
+public class Car {
+     private String engine;
+     private String category;
+     private String wheels;
      private int seat;
-     private String Category;
-     private Lock Locked;
-     public Car1 (Engine engine, Wheel wheels, int seat, String Category, Lock Locked) {
+
+     public Car(String engine, String wheels, int seat, String category){
+          System.out.println("Этот автомобиль ");
+          setValues(engine, wheels,seat, category);
+          System.out.println(displayInfo());
+
+     }
+
+     public void setValues(String engine, String wheels, int seat, String category) {
           this.engine = engine;
           this.wheels = wheels;
           this.seat = seat;
-          this.Category = Category;
-          this.Locked = Locked;
+          this.category = category;
      }
-     abstract void start();
-     public void refuel() {
+     public String displayInfo() {
+          return "Engine: " + engine + "\nCategory: " + category + "\nWheels: " + wheels + "\nSeat: " + seat;
      }
-     public void drive() {
-     }
-     static class Engine {
-          double volume;
-          String fuelType;
-          String name;
-          public Engine(double volume, String fuelType, String name) {
-               this.volume = volume;
-               this.fuelType = fuelType;
-               this.name = name;
-          }
-     }
-     static class Wheel {
-          double radius;
-          String seasonType;
-          String name;
-          public Wheel(double radius, String seasonType, String name) {
-               this.radius = radius;
-               this.seasonType = seasonType;
-               this.name = name;
-          }
-     }
-     static class Lock {
-          String[] keys;
-          public Lock(String[] keys) {
-               this.keys = keys;
-          }
-          public static void main(String[] args) {
-               System.out.println(Car1);}
-          }
-     }
+
 }
---
+++ src/Car/Сar3.java
@@ -0,0 +1,8 @@
+package Car;
+
+public class Сar3 {
+    public static void main(String[] args) {
+        System.out.println();
+        Car Wolwo = new Car("V6","Sheel", 5, "B");
+    }
+}
